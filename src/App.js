b0ddcc697ec7c6818334c3761db5a20ef2ccea76
LindaniMags <lindanimags@gmail.com>
2024-09-12 22:59:52 +0200

    Update hero section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import house from "./images/house.png";
 import baby from "./images/playtime.png";
 import laundry from "./images/laundry-machine.png";
 import clean from "./images/products.png";
+import linked from "./images/linkedin.png";
+import git from "./images/github.png";
 import "./App.css";
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
       <div className="hero">
         <h1>All The Help You Need At Home</h1>
         <h1>We Got You!</h1>
+        <h1>
+          <button>LETS GO!</button>
+        </h1>
       </div>
       <h2>Here Are Some of The Services On Offer.</h2>
       <div className="cubes-con">
@@ -32,7 +37,7 @@ function App() {
           </div>
           <div className="baby-sit-text cubes">
             <h1>ARE YOU IN NEED OF</h1>
-            <h1>SOME SPRING CLEANING</h1>
+            <h1>SOME SPRING CLEANING?</h1>
             <h1>WE GOT YOU COVERED</h1>
           </div>
         </div>
@@ -56,8 +61,28 @@ function App() {
         </div>
       </div>
       <h2>And Many More...</h2>
+      <p>
+        We Live In A Busy Well And We Do Not Always Have The Time To Do Some The
+        Chores That We Have To Do At
+      </p>
+      <p>
+        Home. Let Us Take The Burden Off You So You Can Go On And Do The
+        Important Things.
+      </p>
+      <p>
+        You Now No Longer Have To Worry About Finding The Help You Need, When
+        You Need It
+      </p>
       <footer>
-        <h1>FOOTER</h1>
+        <div className="icons">
+          <a href="">
+            <img src={linked} alt="Linkedin" className="icon" />
+          </a>
+          <a href="">
+            <img src={git} alt="Github" className="icon" />
+          </a>
+        </div>
+        <h3>SITTERS</h3>
       </footer>
     </div>
   );
